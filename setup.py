0d1d06e7e4bd015563c3da69cdc837b01aabb2f4
Elliott Blocha <eblocha@gmail.com>
2021-03-08 16:45:47 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "django_encrypted_files",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Elliott Blocha",
     description = "Encrypt files uploaded to a Django application.",
     license = "MIT",
